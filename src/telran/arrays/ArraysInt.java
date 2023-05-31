4b1f46782e19a623c20b14d30837d6c9bca2267c
Alexander <caxagsh@gmail.com>
2023-05-31 19:47:19 +0300

    hw_02

---
+++ src/telran/arrays/ArraysInt.java
@@ -0,0 +1,35 @@
+package telran.arrays;
+
+import java.util.Arrays;
+
+public class ArraysInt {
+	public static int [] addNumber(int [] array, int number) {
+		int [] res = Arrays.copyOf(array, array.length + 1);
+		res[array.length] = number;     
+		return res;
+		
+	}
+	public static int [] insertNumber (int [] array, int index, int number) {
+		int [] res = new int [array.length + 1];
+		System.arraycopy(array, 0, res, 0, index);
+		res[index] = number;
+		System.arraycopy(array, index, res, index + 1, array.length - index);
+		return res;
+	}
+	public static int [] removeNumber(int [] array, int index) {
+		int [] res = new int [array.length - 1];
+		System.arraycopy(array, 0, res, 0, index);
+		System.arraycopy(array, index + 1, res, index, array.length - index - 1);
+		return res;
+	}
+	public static int[] insertNumberSorted(int [] arraySorted, int number) {
+		int index = Arrays.binarySearch(arraySorted, number);
+		if(index < 0) {
+			index = index * -1 - 1;
+		}
+		
+//		int index = Arrays.binarySearch(arraySorted, number) < 0 ? Arrays.binarySearch(arraySorted, number) * -1 - 1: Arrays.binarySearch(arraySorted, number);
+		
+		return insertNumber(arraySorted, index, number);
+	}
+}
---
+++ src/telran/arrays/test/ArraysTest.java
@@ -0,0 +1,106 @@
+package telran.arrays.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import telran.arrays.ArraysInt;
+
+class ArraysTest {
+
+	@Test
+	void initialTest() {
+		int [] ar1 = {1,2,3};
+		int [] ar2 = {1,2,3};
+		int [] ar2_3 = ar2;
+		assertFalse(ar1 == ar2);
+		assertTrue(ar2 == ar2_3); //check reference equals
+		assertArrayEquals(ar1, ar2); //check array equals
+		ar2_3[0] = 10;
+		ar1[0] = 10;
+		assertArrayEquals(ar1, ar2);
+		
+		
+	}
+@Test
+void copyOfIntTets() {
+	int [] ar1 = {10, 5, 15};
+	int [] expected1 = {10, 5, 15};
+	int [] expected2 = {10, 5};
+	int [] expected3 = {10, 5, 15, 0, 0};
+	assertArrayEquals(expected1, Arrays.copyOf(ar1, 3));
+	assertArrayEquals(expected2, Arrays.copyOf(ar1, 2));
+	assertArrayEquals(expected3, Arrays.copyOf(ar1, 5));
+}
+@Test
+void addNumberTest() {
+	int [] ar1 = {10, 5, 15};
+	int number = 20;
+	int [] expected = {10, 5, 15, number};
+	assertArrayEquals(expected, ArraysInt.addNumber(ar1, number));
+}
+@Test
+void arrayCopyTest() {
+	int [] src = {1, 2, 3, 4, 5, 6, 7};
+	int [] dest = {10, 20, 30 ,40 ,50 ,60, 70};
+	int [] expected = {10, 20, 30, 40, 1, 2, 3};
+	System.arraycopy(src, 0, dest, 4, 3);
+	assertArrayEquals(expected, dest);
+}
+@Test
+void insertNumberTest() {
+	int [] src = {1, 2, 3, 4, 5, 6, 7};
+	int number = 20;
+	int index = 3;
+	int [] expected1 = {1, 2, 3, 20, 4, 5, 6, 7};
+	int [] expected2 = {30, 1, 2, 3, 4, 5, 6, 7};
+	int [] expected3 = {1, 2, 3, 4, 5, 6, 7, 40};
+	assertArrayEquals(expected1, ArraysInt.insertNumber(src, index, number));
+	number = 30;
+	index = 0;
+	assertArrayEquals(expected2, ArraysInt.insertNumber(src, index, number));
+	number = 40;
+	index = 7;
+	assertArrayEquals(expected3, ArraysInt.insertNumber(src, index, number));
+}
+@Test
+void removeNumberTest() {
+	int [] src = {1, 2, 3, 4, 5, 6, 7};
+	int index = 3;
+	int [] expected1 = {1, 2, 3, 5, 6, 7};
+	assertArrayEquals(expected1, ArraysInt.removeNumber(src, index));
+	index = 0;
+	int [] expected2 = { 2, 3, 4, 5, 6, 7};
+	assertArrayEquals(expected2, ArraysInt.removeNumber(src, index));
+	index = 6;
+	int [] expected3 = {1, 2, 3, 4, 5, 6};
+	assertArrayEquals(expected3, ArraysInt.removeNumber(src, index));
+	
+	
+	
+}
+@Test
+void binarySearchTest() {
+	//TODO test for understanding the standard method Arrays.binarySearch(int[] arraysSorted,
+	// int key)
+}
+@Test
+void insertSortedTest() {
+	int [] src = {2, 3, 4, 6, 7, 8, 9, 10};
+	int number = 1;
+	int [] expected1 = {1, 2, 3, 4, 6, 7, 8, 9, 10};
+	assertArrayEquals(expected1, ArraysInt.insertNumberSorted(src, number));
+	number = 5;
+	int [] expected2 = {2, 3, 4, 5, 6, 7, 8, 9, 10};
+	assertArrayEquals(expected2, ArraysInt.insertNumberSorted(src, number));
+	number = 11;
+	int [] expected3 = {2, 3, 4, 6, 7, 8, 9, 10, 11};
+	assertArrayEquals(expected3, ArraysInt.insertNumberSorted(src, number));
+	number = 6;
+	int [] expected4 = {2, 3, 4, 6, 6, 7, 8, 9, 10};
+	assertArrayEquals(expected4, ArraysInt.insertNumberSorted(src, number));
+}
+
+}
